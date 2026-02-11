017838e50cb1ace7111d6b2e17f98b562f588bda
unknown <paulocouto14@gmail.com>
2026-02-11 16:44:42 -0300

    loguinokfront

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/grafeno', (req, res, next) => {
+  res.render('grafeno', { title: 'Grafeno' });
+});
+
+router.post('/grafeno', (req, res, next) => {
+  // Processar login aqui
+  // Por enquanto, apenas redireciona de volta
+  res.redirect('/grafeno');
 });
 
 module.exports = router;
